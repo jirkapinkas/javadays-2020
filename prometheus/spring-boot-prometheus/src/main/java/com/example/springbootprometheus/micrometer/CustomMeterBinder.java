5b4dea67203c9d821dec593801926eccc041cbdb
Jirka Pinkas <jirka.pinkas@gmail.com>
2020-10-31 17:04:16 +0100

    + counter

--- prometheus/spring-boot-prometheus/src/main/java/com/example/springbootprometheus/micrometer/CustomMeterBinder.java
+++ prometheus/spring-boot-prometheus/src/main/java/com/example/springbootprometheus/micrometer/CustomMeterBinder.java
@@ -1,5 +1,6 @@
 package com.example.springbootprometheus.micrometer;
 
+import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.MeterBinder;
@@ -10,14 +11,20 @@ import java.util.Random;
 @Component
 public class CustomMeterBinder implements MeterBinder {
 
+    private Counter counter;
+
     @Override
     public void bindTo(MeterRegistry meterRegistry) {
         Gauge.builder("custom_gauge", this, value -> value.getCustomValue())
                 .description("Custom Gauge")
                 .register(meterRegistry);
+        counter = Counter.builder("custom_counter")
+                .description("Custom Counter")
+                .register(meterRegistry);
     }
 
     private Double getCustomValue() {
+        counter.increment();
         return (double) new Random().nextInt(10);
     }
 
