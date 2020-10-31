47ed34bd20aec599eff1e4e7d1f84e1586b66060
Jirka Pinkas <jirka.pinkas@gmail.com>
2020-10-31 20:33:00 +0100

    + priklad na @Timed

--- prometheus/spring-boot-prometheus/src/main/java/com/example/springbootprometheus/controller/HelloController.java
+++ prometheus/spring-boot-prometheus/src/main/java/com/example/springbootprometheus/controller/HelloController.java
@@ -1,12 +1,14 @@
 package com.example.springbootprometheus.controller;
 
 import com.example.springbootprometheus.pojo.Message;
+import io.micrometer.core.annotation.Timed;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Timed
     @GetMapping("/message")
     public Message message() {
         return new Message("stuff");
