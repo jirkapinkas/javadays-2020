63395f599f91167849bc277622008cf26bbfbe13
Jirka Pinkas <jirka.pinkas@gmail.com>
2020-11-01 15:40:57 +0100

    fix trapne chybicky z prednasky

--- spring-data-rest/spring-data-rest-hello-world/src/main/java/com/example/helloworld/repository/priklad1/DogRepository.java
+++ spring-data-rest/spring-data-rest-hello-world/src/main/java/com/example/helloworld/repository/priklad1/DogRepository.java
@@ -3,6 +3,11 @@ package com.example.helloworld.repository.priklad1;
 import com.example.helloworld.entity.priklad1.Dog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 //@RepositoryRestResource(collectionResourceRel = "pets", path = "pets")
 public interface DogRepository extends JpaRepository<Dog, Long> {
+
+    List<Dog> findByName(String name);
+
 }
