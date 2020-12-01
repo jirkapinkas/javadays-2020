89c7f846f0f585904e5abd839a3b792bb8c8db4f
Jirka Pinkas <jirka.pinkas@gmail.com>
2020-12-01 12:06:31 +0100

    upgrade na Spring Boot 2.4. Musela se udelat drobna zmena v konfiguraci!!!

--- spring-data-rest/spring-data-rest-hello-world/src/main/java/com/example/helloworld/RestApiConfiguration.java
+++ spring-data-rest/spring-data-rest-hello-world/src/main/java/com/example/helloworld/RestApiConfiguration.java
@@ -3,10 +3,12 @@ package com.example.helloworld;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.data.web.HateoasPageableHandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import javax.persistence.EntityManager;
 import javax.persistence.metamodel.Type;
@@ -17,9 +19,12 @@ public class RestApiConfiguration implements RepositoryRestConfigurer {
     @Autowired
     private EntityManager entityManager;
 
+    /*
+     * Tato beana overriduje beanu pageableResolver v RepositoryRestMvcConfiguration !!!
+     */
+    @Primary
     @Bean
-    public HateoasPageableHandlerMethodArgumentResolver customResolver(
-            HateoasPageableHandlerMethodArgumentResolver pageableResolver) {
+    public HateoasPageableHandlerMethodArgumentResolver customResolver(HateoasPageableHandlerMethodArgumentResolver pageableResolver) {
         pageableResolver.setOneIndexedParameters(true);
         pageableResolver.setFallbackPageable(PageRequest.of(0, 20));
         pageableResolver.setMaxPageSize(Integer.MAX_VALUE);
@@ -27,7 +32,7 @@ public class RestApiConfiguration implements RepositoryRestConfigurer {
     }
 
     @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry corsRegistry) {
         // https://stackoverflow.com/questions/30912826/expose-all-ids-when-using-spring-data-rest
         config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream()
                 .map(Type::getJavaType)
